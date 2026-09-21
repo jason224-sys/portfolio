43b06e0d9540605e203bb62a8fd3cfd49ca6fe5a
jason224-sys <jason2241991@outlook.com>
2026-09-21 01:02:48 -0500

    Deploy link vercel

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -22,7 +22,7 @@ const projects = [
     link: "https://res.cloudinary.com/skpz5si6/image/upload/f_auto,q_auto/portfolio",
     color: "#8f89ff",
     githubLink: "https://github.com/jason224-sys/portfolio",
-    liveLink: "https://portfolio-axlw6fq4x-jason2241991-1747.vercel.app/",
+    liveLink: "https://portfolio-eight-iota-km2647b6de.vercel.app/",
   },
   {
     title: "PimEyes",
