8c01bd8498b15cf42d153424ebb614b816e1a227
jason224-sys <jason2241991@outlook.com>
2026-09-21 07:29:00 -0500

    resume link

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -22,7 +22,7 @@ const projects = [
     link: "https://res.cloudinary.com/skpz5si6/image/upload/v1789984735/portfolio.png",
     color: "#8f89ff",
     githubLink: "https://github.com/jason224-sys/portfolio",
-    liveLink: "/#",
+    liveLink: "#",
   },
   {
     title: "PimEyes",
